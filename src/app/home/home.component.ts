616e63dd1831724ed493b66e319cee6e55287e13
Atif Nabil <atif.nabil@modiscloud.net>
2020-06-09 14:59:47 +0200

    refacto

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {MarvelService} from '../_service/marvel.service';
-import {Observable} from 'rxjs';
-import {MarvelObject} from '../_model/MarvelObject';
 
 @Component({
   selector: 'app-home',
@@ -16,9 +14,7 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // console.log('toto2');
     this.marvelService.allCharacter().subscribe((res) => {
-      console.log('toto' + res.data.results[2].thumbnail.path);
       this.allCharacter = res.data.results;
     });
   }
